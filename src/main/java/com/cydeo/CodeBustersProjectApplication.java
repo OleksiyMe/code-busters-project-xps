a209720403153d347ab6bb6574174e550e9a26d3
Sumeyse <sumeyse@gmail.com>
2022-12-23 18:14:39 +0300

    first commit

--- src/main/java/com/cydeo/CodeBustersProjectApplication.java
+++ src/main/java/com/cydeo/CodeBustersProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyAccountingProjectApplication {
+public class CodeBustersProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyAccountingProjectApplication.class, args);
+        SpringApplication.run(CodeBustersProjectApplication.class, args);
     }
 
 }
