7139afaf4b8b200b91e6e8fc407f15c8f94cd1f2
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-12-27 15:36:32 -0500

    fixed run errors (added fields to ClientVendor, added enum ClientVendorType)

--- src/main/java/com/cydeo/entity/ClientVendor.java
+++ src/main/java/com/cydeo/entity/ClientVendor.java
@@ -1,9 +1,25 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ClientVendorType;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "clients_vendors")
 public class ClientVendor extends BaseEntity {
+
+    private String clientVendorName;
+    @Enumerated(EnumType.STRING)
+    private ClientVendorType clientVendorType;
+    private String phone;
+    private String website;
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
 }
