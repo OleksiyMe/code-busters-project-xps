43482986ddfa367fc6b8f6b799ceae3dbadfa887
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-12-27 15:54:05 -0500

    fixed run errors (added fields to ClientVendor, added enum ClientVendorType)

--- src/main/java/com/cydeo/enums/ClientVendorType.java
+++ src/main/java/com/cydeo/enums/ClientVendorType.java
@@ -1,5 +1,12 @@
 package com.cydeo.enums;
 
 public enum ClientVendorType {
-    CLIENT, VENDOR
+    VENDOR("Vendor"),
+    CLIENT("Client");
+
+    private final String value;
+
+    ClientVendorType(String value) {
+        this.value = value;
+    }
 }
