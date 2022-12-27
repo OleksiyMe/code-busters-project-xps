ed29f13cc265be423bd186d1a5cda1e8b428aac6
Hrvoje <hbrkljacic@gmail.com>
2022-12-27 18:13:01 -0500

    UserDtoConverter done

--- src/main/java/com/cydeo/converter/UserDtoConverter.java
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -1,15 +1,25 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.UserDto;
+import com.cydeo.service.UserService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDtoConverter implements Converter<String, UserDto> {
 
+    private final UserService userService;
+
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDto convert(String source) {
-        return null;
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return userService.findByUsername(source);
     }
 }
