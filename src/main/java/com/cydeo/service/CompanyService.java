be938e9266df7fac88f9e23b95fb4097a91b2606
DianaSagoyan <dianasagoyan@yahoo.com>
2022-12-28 13:20:44 -0600

    Created Crud methods for Company Service

--- src/main/java/com/cydeo/service/CompanyService.java
+++ src/main/java/com/cydeo/service/CompanyService.java
@@ -2,7 +2,14 @@ package com.cydeo.service;
 
 import com.cydeo.dto.CompanyDto;
 
+import java.util.List;
+
 public interface CompanyService {
-    CompanyDto findById(Long id);
+    CompanyDto findCompanyById(Long id);
+
+    void createCompany(CompanyDto company);
+    List<CompanyDto> readAllCompanies();
+    CompanyDto updateCompany(Long id);
+    void deleteCompany(Long id);
 
 }
