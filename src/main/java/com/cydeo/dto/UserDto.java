0282bd023fe92430336334c923b8b14744528b15
Umran Ozman <umonal@yahoo.com>
2022-12-30 11:31:21 -0500

    JDZH8-235-UO /Added the Validation to User DTO-US 231

--- src/main/java/com/cydeo/dto/UserDto.java
+++ src/main/java/com/cydeo/dto/UserDto.java
@@ -3,21 +3,49 @@ package com.cydeo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class UserDto {
 
     Long id;
+
+    @NotBlank (message = "User Name is a required field.")
+    @Email(message = "Email is a required field.")
     String username;
+
+    @NotBlank (message = "Password is a required field.")
+    @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
     String password;
+
+    @NotBlank (message = "Password needs to match")
+    @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
     String confirmPassword;
+
+    @NotBlank(message = "First Name is a required field.")
+    @Size(max = 50, min = 2, message = "First Name should be 2-50 characters long.")
     String firstname;
+
+    @NotBlank(message = "Last Name is a required field.")
+    @Size(max = 50, min = 2, message = "Last Name should be 2-50 characters long.")
     String lastname;
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$" // +111 (202) 555-0125  +1 (202) 555-0125
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"                                  // +111 123 456 789
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$", message = "Phone number is required field and may be in any valid phone number format.")
     String phone;
+
+    @NotBlank (message = "Please select a role")
     RoleDto role;
+    @NotBlank (message = "Please select a customer")
     CompanyDto company;
+
     Boolean isOnlyAdmin;
+
+
+
+
 
 }
