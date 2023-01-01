290fda08e82a50c88852bbf21da7b63351bbfcb3
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-12-31 20:28:26 -0500

    JDZH8-236 (CreateClientVendor method in ClientVendorController)

--- src/main/java/com/cydeo/dto/ClientVendorDto.java
+++ src/main/java/com/cydeo/dto/ClientVendorDto.java
@@ -3,18 +3,40 @@ package com.cydeo.dto;
 import com.cydeo.enums.ClientVendorType;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientVendorDto {
 
-    Long id;
-    String clientVendorName;
-    String phone;
-    String website;
-    ClientVendorType clientVendorType;
-    AddressDto address;
-    CompanyDto company;
+    private Long id;
+
+    @NotBlank(message = "Company Name is a required field.")
+    @Size(max = 50, min = 2, message = "Company Name should be 2-50 characters long.")
+    private String clientVendorName;
+
+    @NotBlank(message = "Phone is a required field.")
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$" // +111 (202) 555-0125  +1 (202) 555-0125
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"                                  // +111 123 456 789
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$",
+            message = "Phone number is required field and may be in any valid phone number format.")
+    // +111 123 45 67 89
+    private String phone;
+
+    @NotBlank(message = "Website is a required field.")
+    @Size(max = 100, min = 2, message = "Website should be 2-100 characters long.")
+    @Pattern(regexp = "^http(s{0,1})://[a-zA-Z0-9/\\-\\.]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9/\\&\\?\\=\\-\\.\\~\\%]*",
+            message = "Website should have a valid format.")
+    private String website;
+
+    @NotNull(message = "Please select type.")
+    private ClientVendorType clientVendorType;
+    private AddressDto address;
+    private CompanyDto company;
 
 }
