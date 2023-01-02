90c8abe91320b72a7e916d0a5f189d1276444340
Umran Ozman <umonal@yahoo.com>
2023-01-02 13:49:08 -0500

    JDZH8-246-UO / Product create + Validation is completed

--- src/main/java/com/cydeo/dto/ProductDto.java
+++ src/main/java/com/cydeo/dto/ProductDto.java
@@ -3,6 +3,11 @@ package com.cydeo.dto;
 import com.cydeo.enums.ProductUnit;
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -10,10 +15,21 @@ import lombok.*;
 public class ProductDto {
 
     Long id;
+
+    @NotBlank
+    @Size(max = 100, min = 2)
     String name;
+
     Integer quantityInStock;
+
+    @NotNull
+    @Min(value=1)
     Integer lowLimitAlert;
+
+    @NotNull
     ProductUnit productUnit;
+
+    @NotNull
     CategoryDto category;
 
 }
