04eaaa76af7a51acd21f1e8f4e1b0fa7050ca766
Oleksiy Melnychuk <melnichuk@gmail.com>
2023-01-02 12:37:17 -0500

    corrected regex for password, to comply the requirements

--- src/main/java/com/cydeo/dto/UserDto.java
+++ src/main/java/com/cydeo/dto/UserDto.java
@@ -19,7 +19,11 @@ public class UserDto {
     String username;
 
     @NotBlank(message = "Password is a required field.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$",
+//    @Pattern(regexp = "^(?=.*([0-9])(?=.*[a-z])(?=.*[A-Z])" +
+//            "(?=.*[\\\\~!()/@#$%^&+=*])(?=\\S+$).{4,}$",
+//    @Pattern(regexp = "^(?=.*[!-@])(?=.*[\\[-_])(?=.*[{-~])(?=.*[a-z])(?=.*[A-Z]).{4,}$",
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])" +
+                    "(?=.*([ -/]|[:-@]|[Z-_]|[{-~]))(?=\\S+$).{4,}$",
             message = "Password must contain at least: " +
                     "1 lowercase, " +
                     "1 uppercase, " +
@@ -28,7 +32,6 @@ public class UserDto {
     private String password;
 
     @NotBlank (message = "Password needs to match")
-
     String confirmPassword;
 
     @NotBlank(message = "First Name is a required field.")
